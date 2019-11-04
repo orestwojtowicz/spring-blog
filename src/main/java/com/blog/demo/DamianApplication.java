c585b9e91bec9d2811179f6b2774c84a8673cedd
damiass <orestwojtowicz94@gmail.com>
2019-11-04 22:44:15 +0100

    css changes on comment section

--- src/main/java/com/blog/demo/DamianApplication.java
+++ src/main/java/com/blog/demo/DamianApplication.java
@@ -1,9 +1,7 @@
 package com.blog.demo;
 
-import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
